469c86824fab1143bb7940f9af2bd69681ed8bc8
Gerner Péter <petergerner@users.noreply.github.com>
2019-11-22 17:39:37 +0100

    add clean-up script (#1)
    add clean-up script to post-update-cmd

---
+++ scripts/bash/remove-builtin-profiles.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# remove built-in D8 profiles
+rm -rf web/core/profiles/demo_umami
+rm -rf web/core/profiles/standard
+rm -rf web/core/profiles/minimal
+rm -rf web/core/profiles/testin*
